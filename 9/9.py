958f6d0f3cedf6ab82f9b3958e8ccc9341c61b7c
iLukez <luca.donadel05@gmail.com>
2022-12-09 23:55:09 +0100

    Improvements to day 9's code

--- 9/9.py
+++ 9/9.py
@@ -1,5 +1,5 @@
 def findDistance(pos_h, pos_t):
-    return abs(max(abs(pos_h[0] - pos_t[0]), abs(pos_h[1] - pos_t[1])))
+    return max(abs(pos_h[0] - pos_t[0]), abs(pos_h[1] - pos_t[1]))
 
 def move_diagonally(pos_move, pos_reach):
     if pos_reach[0] > pos_move[0] and pos_reach[1] > pos_move[1]:
@@ -14,25 +14,6 @@ def move_diagonally(pos_move, pos_reach):
     if pos_reach[0] > pos_move[0] and pos_reach[1] < pos_move[1]:
         pos_move[0] += 1
         pos_move[1] -= 1
-
-def showGraphic(pos):
-    grid = [[] for i in range(0, 12)]
-    for i in reversed(range(0, 7)):
-        grid[i] = ""
-        buffer = []
-        for j in reversed(range(0, 12)):
-            buffer.append(".")
-        grid[i] = buffer
-    
-    for i, knot in enumerate(pos):
-        grid[knot[0]][knot[1]] = "#"
-        if i == 0:
-            grid[knot[0]][knot[1]] = "H"
-    
-    for line in reversed((grid)):
-        for char in reversed(line):
-            print(char, end=" ")
-        print()
 
 def part1(data):
     moves = [tuple(line.split(" ")) for line in data.split("\n")]
@@ -78,7 +59,7 @@ def part2(data):
                 if findDistance(pos[j - 1], pos[j]) >= 2 and (pos[j-1][0] == pos[j][0] or pos[j-1][1] == pos[j][1]):
                     pos[j] = prev_pos[j-1]
                 elif findDistance(pos[j - 1], pos[j]) >= 2:
-                    move_diagonally(pos[j], pos[j - 1])
+                    move_diagonally(pos[j], pos[j - 1]) 
             visited_pos.add(tuple(pos[9]))
     return len(visited_pos)
 
@@ -87,4 +68,4 @@ with open("input.txt") as f:
     
 print("1:", part1(data))
 print("2:", part2(data))
-print("Warning: solution for Part 2 is currently wrong")
+print("Solution for Part 2 is currently wrong")
